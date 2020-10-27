7b167d8ca5cf239d20bccd513773c8d43f3902bc
Pina <brannoncpina@gmail.com>
2020-10-26 22:00:11 -0600

    Added clear button, updated README, and added comments. Finished

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # BPina_hw6
+This is my "Weather Dashboard". Any city can be searched to get the current weather as well as a five day forecast. Any searched city will appear on the left for an easy search later. If the page is refreshed the cities that have been searched will still appear as well as the forecast of the last searched city.
+
+Weather Dashboard - https://bcpbyu.github.io/BPina_hw6/
+
+## Pictures
+
+This is the page first opened
+
+![Capture 1](./Assets/firstOpen.png)
+
+This is the page when a city is searched
+
+![Capture 2](./Assets/citySearch.png)
+
+This is the page when refreshed with mulitple past searches
+
+![Capture 3](./Assets/multiSearch.png)
--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+// Global variables
 var rnWeather = $("#rnWeather");
-var rnURL = "https://api.openweathermap.org/data/2.5/weather?";
-var daysURL = "https://api.openweathermap.org/data/2.5/onecall?";
-var uvURL = "http://api.openweathermap.org/data/2.5/uvi?";
-var APIKey = "18409c975f3793d3225bf3a92db4ca88";
 var cities = [0];
 var savedCities = [];
 var lat;
 var lon;
 var city;
 
+// API variables
+var rnURL = "https://api.openweathermap.org/data/2.5/weather?";
+var daysURL = "https://api.openweathermap.org/data/2.5/onecall?";
+var uvURL = "http://api.openweathermap.org/data/2.5/uvi?";
+var APIKey = "18409c975f3793d3225bf3a92db4ca88";
 
+// Call load function
+load();
+
+// Event listener for search button
 $("#search").on("click", function () {
     rnWeather.empty();
     city = $("#cityInput").val().trim();
@@ -19,8 +25,24 @@ $("#search").on("click", function () {
     console.log(cities);
 });
 
+// Event listener for city buttons
+$(document).on("click", "#cityButton", function(){
+    city = $(this).attr("data-city");
+    rnWeather.empty();
+    for (var i = 1; i < 6; i++){
+        $("#day"+i).empty();
+    }
+    weatherRn();
+});
+
+// Event listener for clear button
+$("#clear").on("click", function(){
+    localStorage.removeItem("cities");
+    location.reload();
+});
+
+// Function to search and display the city's forecast
 function weatherRn() {
-    
     var query1URL = rnURL + "q=" + city + "&appid=" + APIKey;
     $.ajax({
         url: query1URL,
@@ -63,10 +85,11 @@ function weatherRn() {
                 $("<p>").text("Temp: " + ((response.daily[i].temp.day-273.15)*1.8+32).toFixed(1) + " °F").appendTo("#day" + i);
                 $("<p>").text("Humidity: " + response.daily[i].humidity + "%").appendTo("#day" + i);
             }
-        })
+        });
     });
 }
 
+// Function to create the list of past searched cities
 function cityList () {
     var cityAdd = $("<li>").text(city).attr("data-city",city);
     cityAdd.attr("id", "cityButton");
@@ -79,8 +102,7 @@ function cityList () {
     console.log("saved: " + JSON.parse(localStorage.getItem("cities")));
 }
 
-load();
-
+// Function to create list of past searched saved cities
 function load() {
     var localCities = JSON.parse(localStorage.getItem("cities"));
     if (localCities != null){
@@ -97,11 +119,3 @@ function load() {
     weatherRn();
 }
 
-$(document).on("click", "#cityButton", function(){
-    city = $(this).attr("data-city");
-    rnWeather.empty();
-    for (var i = 1; i < 6; i++){
-        $("#day"+i).empty();
-    }
-    weatherRn();
-});
